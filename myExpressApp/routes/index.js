fc16d983a4def0ad15853296ba2cba8787f9f9a8
19MIC0079 <hariharanvinayagam7@gmail.com>
2022-08-25 16:22:17 +0530

    initial commit

---
+++ myExpressApp/routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'HARIHARAN-19MIC0079' });
+});
+
+module.exports = router;
